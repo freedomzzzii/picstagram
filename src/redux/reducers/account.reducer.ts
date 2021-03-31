f850c5531143ff689fe9d0cbd2ca255128d9784e
freedomzzzii <theeraporn.tia@gmail.com>
2021-04-01 03:14:54 +0700

    feat: create reducer get profile

---
+++ src/redux/reducers/account.reducer.ts
@@ -0,0 +1,15 @@
+import constant from '../../common/constant';
+import { actionDefaultType } from '../../common/type'
+
+export const getProfile = (state = constant.initialState, action: actionDefaultType) => {
+  switch (action.type) {
+    case constant.GET_PROFILE_REQUEST:
+      return Object.assign({}, state, { ...action });
+    case constant.GET_PROFILE_SUCCESS:
+      return Object.assign({}, state, { ...action });
+    case constant.GET_PROFILE_FAILURE:
+      return Object.assign({}, state, { ...action });
+    default:
+      return state ? state : constant.initialState;
+  }
+};
