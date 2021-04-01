2c3f471e2bec9148e69c30a0ec411e9a76b914d5
freedomzzzii <theeraporn.tia@gmail.com>
2021-04-02 03:21:49 +0700

    feat: create reducer get list photo

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -4,9 +4,13 @@ import { getProfile } from './account.reducer';
 
 import { loading } from './loading.reducer';
 
+import { getListPhoto } from './photo.reducers';
+
 export default combineReducers({
   // loading
   loading,
   // account
   getProfile,
+  // photo
+  getListPhoto,
 });
