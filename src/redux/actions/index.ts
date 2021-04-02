223ffc8f8c33af4e5d5707490f9d9c9a9828a667
freedomzzzii <theeraporn.tia@gmail.com>
2021-04-02 17:32:15 +0700

    feat: create action search user

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -2,14 +2,17 @@ import { createAction } from 'redux-actions';
 
 import constant from '../../common/constant';
 
-// score
+// account
 const fetchGetProfile = createAction(constant.GET_PROFILE_REQUEST);
+const fetchSearchUsers = createAction(constant.GET_SEARCH_USERS_REQUEST);
 
 // photo
 const fetchGetListPhoto = createAction(constant.GET_LIST_PHOTO_REQUEST);
 
 export {
-  // score
+  // account
   fetchGetProfile,
+  fetchSearchUsers,
+  // photo
   fetchGetListPhoto,
 };
