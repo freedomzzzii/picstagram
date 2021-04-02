e22cc1f08cefbad90e4b424b78522c8df8fa55a0
freedomzzzii <theeraporn.tia@gmail.com>
2021-04-02 22:22:11 +0700

    feat: create action for post user page

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -5,14 +5,18 @@ import constant from '../../common/constant';
 // account
 const fetchGetProfile = createAction(constant.GET_PROFILE_REQUEST);
 const fetchSearchUsers = createAction(constant.GET_SEARCH_USERS_REQUEST);
+const fetchGetProfileByUser = createAction(constant.GET_PROFILE_BY_USER_REQUEST);
 
 // photo
 const fetchGetListPhoto = createAction(constant.GET_LIST_PHOTO_REQUEST);
+const fetchGetListPhotoByUser = createAction(constant.GET_LIST_PHOTO_BY_USER_REQUEST);
 
 export {
   // account
   fetchGetProfile,
   fetchSearchUsers,
+  fetchGetProfileByUser,
   // photo
   fetchGetListPhoto,
+  fetchGetListPhotoByUser,
 };
